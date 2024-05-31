3dced7f647373167c7b02aca1917131caf09b328
KirillBob <Kirill.Bobrov.72@mail.ru>
2024-05-31 22:08:46 +0300

    Film.h

---
+++ Film.h
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+class Film
+{
+public:
+    int id{};
+    string genre{};
+    string name{};
+    string producer{};
+    string country{};
+    int year{};
+    int rating{};
+    string actor{};
+    int cost{};
+    int boxOffice{};
+};
